44f6d3b264bd867f203bf0c7bf42d0c3b08f55bb
adnaanism <78425049+adnaanism@users.noreply.github.com>
2021-02-06 21:39:52 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    float a,b;
+    scanf("%d%d",&a,&b);
+    printf("enter 2 number whose operations you want(a and b)");
+    printf("a+b=%f",a+b);
+    printf("a-b=%f",a-b);
+    printf("a*b=%f",a*b);
+    printf("a/b=%f",a/b);
+
+
+    return 0;
+}
---
+++ Q5 (1).c
@@ -0,0 +1,37 @@
+/**
+ * C program to check vowel or consonant using switch case
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    
+    printf("Enter any alphabet: ");
+    scanf("%c", &ch);
+
+    switch(ch)
+    {
+        case 'a': 
+            printf("Vowel");
+            break;
+        case 'e': 
+            printf("Vowel");
+            break;
+        case 'i': 
+            printf("Vowel");
+            break;
+        case 'o': 
+            printf("Vowel");
+            break;
+        case 'u': 
+            printf("Vowel");
+            break;
+        default: 
+            printf("Consonant");
+    }
+
+    return 0;
+}
